afe77d0d61efb8313ac0faac4f93b2c3f6b8614f
Oleg Solovyev <oleg.s.solovyev@gmail.com>
2021-09-28 20:10:09 +0300

    removed secret key fixed some html

--- artel/settings.py
+++ artel/settings.py
@@ -21,7 +21,6 @@ TEMPLATES_DIR= os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rd+=tx#0_dj2b61q)b@kim6+@kmj*tscpx0%p*$&k9lwnuo*)6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
